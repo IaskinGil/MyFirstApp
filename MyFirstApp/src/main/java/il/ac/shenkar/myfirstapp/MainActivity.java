b7ac0bd31805a17bbcd604fee11bc919aed01bdc
Gil Iaskin <giliaskin@ymail.com>
2013-11-04 21:19:45 +0200

    init

---
+++ MyFirstApp/src/main/java/il/ac/shenkar/myfirstapp/MainActivity.java
@@ -0,0 +1,35 @@
+package il.ac.shenkar.myfirstapp;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "il.ac.shenkar.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
